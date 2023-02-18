27011173e24dda6a30f68a539e40578cb269d89b
Tamas Levai <levait@tmit.bme.hu>
2023-02-18 17:04:17 +0100

    Add switch/richter-scale exercise

---
+++ 07-switch/01-richter-scale/exercise.go
@@ -0,0 +1,9 @@
+package richterscale
+
+//go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+// describeEarthquake prints the "description" of a given magnitude value on the Richter scale
+// NOTE: use the switch statement for table lookup
+func describeEarthquake(magnitude float32) string {
+	// INSERT YOUR CODE HERE
+}

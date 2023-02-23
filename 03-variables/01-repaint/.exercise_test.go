589cfc32023b5d7e60c88aa3e4187c2b984e4b08
Tamas Levai <levait@tmit.bme.hu>
2023-02-23 17:00:54 +0100

    Add variables/repaint exercise

---
+++ 03-variables/01-repaint/.exercise_test.go
@@ -0,0 +1,11 @@
+package repaint
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepaintColor(t *testing.T) {
+	assert.Equal(t, repaintColor({{index . "primary"}}), "{{index . "complementary"}}")
+}
---
+++ 03-variables/01-repaint/exercise.go
@@ -0,0 +1,11 @@
+package repaint
+
+//go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+// repaintColor assigns the complementary color to the `color` variable
+func repaintColor(c string) string {
+	color := c
+	// INSERT YOUR CODE HERE
+
+	return color
+}

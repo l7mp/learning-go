95aae5f041ca550bdaacd1b16ac343ff5f58d19f
Gabor Retvari <retvari@tmit.bme.hu>
2023-04-19 15:44:12 +0200

    Minor changes

--- 03-variables/02-path-split/exercise.go
+++ 03-variables/02-path-split/exercise.go
@@ -5,7 +5,7 @@ import "path"
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// splitPath returns the {{index. component}} component of a file path
+// splitPath returns the {{index . component}} component of a file path
 // Note: feel free to  use path.Split(): https://pkg.go.dev/path#Split
 func splitPath(fullpath, component string) string {
 	_ := component

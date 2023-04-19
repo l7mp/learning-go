0ce0460d14d57f3d42ca02816e12d9bc4d0ba88d
Gabor Retvari <retvari@tmit.bme.hu>
2023-04-19 16:45:20 +0200

    Slightly rework 03/repaint

--- 03-variables/01-repaint/.exercise_test.go
+++ 03-variables/01-repaint/.exercise_test.go
@@ -7,5 +7,15 @@ import (
 )
 
 func TestRepaintColor(t *testing.T) {
-	assert.Equal(t, repaintColor("{{index . "primary"}}"), "{{index . "complementary"}}")
+	c, err := repaintColor("{{index . "primary"}}")
+	assert.Equal(t, c, "{{index . "complementary"}}")
+	assert.NoError(t, err)
+
+	c, err = repaintColor("{{index . "complementary"}}")
+	assert.Equal(t, c, "{{index . "primary"}}")
+	assert.NoError(t, err)
+
+	c, err = repaintColor("xxx")
+	assert.Equal(t, c, "")
+	assert.Error(t, err)
 }
--- 03-variables/01-repaint/exercise.go
+++ 03-variables/01-repaint/exercise.go
@@ -1,9 +1,11 @@
 package repaint
 
+import "fmt"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// repaintColor returns the complementary of `color`
-func repaintColor(color string) string {
+// repaintColor returns the complementary of the color received as argument or an error for an unknown error.
+func repaintColor(color string) (string, error) {
 	// INSERT YOUR CODE HERE
 }

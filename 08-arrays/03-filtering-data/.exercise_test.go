6b5e00448e141cde972f1777467ae7953c4e7d12
Gabor Retvari <retvari@tmit.bme.hu>
2023-08-21 23:36:16 +0200

    arrays fixed

--- 08-arrays/03-filtering-data/.exercise_test.go
+++ 08-arrays/03-filtering-data/.exercise_test.go
@@ -6,6 +6,107 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFilteringData(t *testing.T) {
-	assert.EqualValues(t, filteringData({{index . "keys"}}, {{index . "values"}}), mySolution({{index . "keys"}}, {{index . "values"}}))
+var keys []string
+var indices []int
+
+func TestFilterData(t *testing.T) {
+	// even
+	{{if eq (index . "name") "even"}}
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	assert.Equal(t, [10]string{"b", "d", "f", "h", "j", "", "", "", "", ""}, filterData(keys, indices))
+
+	keys = []string{"a", "b"}
+	indices = []int{0, 1}
+	assert.Equal(t, [10]string{"b", "", "", "", "", "", "", "", "", ""}           , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+       	assert.Equal(t, [10]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}  , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}	      , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}	      , filterData(keys, indices))
+
+	{{end}}
+
+	// odd
+	{{if eq (index . "name") "odd"}}
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	assert.Equal(t, [10]string{"a", "c", "e", "g", "i", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	keys = []string{"a", "b"}
+	indices = []int{0, 1}
+	assert.Equal(t, [10]string{"a", "", "", "", "", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	assert.Equal(t, [10]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	{{end}}
+
+	// x > 5
+	{{if eq (index . "name") "greater"}}
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	assert.Equal(t, [10]string{"a", "b", "c", "d", "e", "f", "", "", "", ""}    , filterData(keys, indices))
+
+	keys = []string{"a", "b"}
+	indices = []int{0, 1}
+	assert.Equal(t, [10]string{"a", "b", "", "", "", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	assert.Equal(t, [10]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	assert.Equal(t, [10]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}	    , filterData(keys, indices))
+
+	{{end}}			      	 									  
+
+	// x <= 4
+	{{if eq (index . "name") "smaller"}}
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	assert.Equal(t, [10]string{"f", "g", "h", "i", "j", "", "", "", "", ""}, filterData(keys, indices))
+
+	keys = []string{"a", "b"}
+	indices = []int{0, 1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}     , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}     , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}     , filterData(keys, indices))
+
+	keys = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}
+	indices = []int{1}
+	assert.Equal(t, [10]string{"", "", "", "", "", "", "", "", "", ""}     , filterData(keys, indices))
+
+	{{end}}			      	 									  
 }
--- 08-arrays/03-filtering-data/exercise.go
+++ 08-arrays/03-filtering-data/exercise.go
@@ -3,7 +3,7 @@ package filteringdata
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// nArithmetic returns the result of an arithmetic operation over "n" elements
-func filteringData(keys [30]string, values [30]int) [20]string {
+// filterData filters a slice based in an index slice.
+func filterData(keys []string, indices []int) [10]string {
 	// INSERT YOUR CODE HERE
 }

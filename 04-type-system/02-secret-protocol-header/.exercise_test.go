5ec00b7480913559f6961fe6a5eca53510d5f127
Gabor Retvari <retvari@tmit.bme.hu>
2023-08-21 20:43:07 +0200

    header

--- 04-type-system/02-secret-protocol-header/.exercise_test.go
+++ 04-type-system/02-secret-protocol-header/.exercise_test.go
@@ -7,5 +7,12 @@ import (
 )
 
 func TestCreatePublishFixHeader(t *testing.T) {
-	assert.Equal(t, createPublishFixHeader({{index . "fa" "val"}}, {{index . "bc" "val"}}, {{index . "sc" "val"}}), mySolution({{index . "fa" "val"}}, {{index . "bc" "val"}}, {{index . "sc" "val"}}, {{index . "qos" "val"}}))
+	assert.Equal(t, {{index . "sol0"}}, createPublishFixHeader(false, false, false))
+	assert.Equal(t, {{index . "sol1"}}, createPublishFixHeader(false, false, true))
+	assert.Equal(t, {{index . "sol2"}}, createPublishFixHeader(false, true, false))
+	assert.Equal(t, {{index . "sol3"}}, createPublishFixHeader(false, true, true))
+	assert.Equal(t, {{index . "sol4"}}, createPublishFixHeader(true, false, false))
+	assert.Equal(t, {{index . "sol5"}}, createPublishFixHeader(true, false, true))
+	assert.Equal(t, {{index . "sol6"}}, createPublishFixHeader(true, true, false))
+	assert.Equal(t, {{index . "sol7"}}, createPublishFixHeader(true, true, true))
 }
--- 04-type-system/02-secret-protocol-header/exercise.go
+++ 04-type-system/02-secret-protocol-header/exercise.go
@@ -3,7 +3,7 @@ package secretprotocolheader
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// createPublishFixHeader constructs an octet (8-bit long byte) based on its three arguments and the fix QoS setting
-func createPublishFixHeader(isFirstAttempt byte, isBroadcasted byte, isSecure byte) byte {
+// createPublishFixHeader constructs an octet (8-bit long byte) based on its three arguments and the fix QoS setting.
+func createPublishFixHeader(isFirstAttempt bool, isBroadcasted bool, isSecure bool) byte {
 	// INSERT YOUR CODE HERE
 }

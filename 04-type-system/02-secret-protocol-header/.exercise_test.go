a02638a836eb2897e22a7c27b2db9da74b4fbaa0
Gabor Retvari <retvari@tmit.bme.hu>
2023-08-21 20:48:49 +0200

    protocol header

--- 04-type-system/02-secret-protocol-header/.exercise_test.go
+++ 04-type-system/02-secret-protocol-header/.exercise_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestCreatePublishFixHeader(t *testing.T) {
-	assert.Equal(t, {{index . "sol0"}}, createPublishFixHeader(false, false, false))
-	assert.Equal(t, {{index . "sol1"}}, createPublishFixHeader(false, false, true))
-	assert.Equal(t, {{index . "sol2"}}, createPublishFixHeader(false, true, false))
-	assert.Equal(t, {{index . "sol3"}}, createPublishFixHeader(false, true, true))
-	assert.Equal(t, {{index . "sol4"}}, createPublishFixHeader(true, false, false))
-	assert.Equal(t, {{index . "sol5"}}, createPublishFixHeader(true, false, true))
-	assert.Equal(t, {{index . "sol6"}}, createPublishFixHeader(true, true, false))
-	assert.Equal(t, {{index . "sol7"}}, createPublishFixHeader(true, true, true))
+	assert.Equal(t, byte({{index . "sol0"}}), createPublishFixHeader(false, false, false))
+	assert.Equal(t, byte({{index . "sol1"}}), createPublishFixHeader(false, false, true))
+	assert.Equal(t, byte({{index . "sol2"}}), createPublishFixHeader(false, true, false))
+	assert.Equal(t, byte({{index . "sol3"}}), createPublishFixHeader(false, true, true))
+	assert.Equal(t, byte({{index . "sol4"}}), createPublishFixHeader(true, false, false))
+	assert.Equal(t, byte({{index . "sol5"}}), createPublishFixHeader(true, false, true))
+	assert.Equal(t, byte({{index . "sol6"}}), createPublishFixHeader(true, true, false))
+	assert.Equal(t, byte({{index . "sol7"}}), createPublishFixHeader(true, true, true))
 }

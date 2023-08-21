9940224efb3e00f306b44f0c5ccd06634d02bb36
Gabor Retvari <retvari@tmit.bme.hu>
2023-08-21 22:13:19 +0200

    fact-or-sum

--- 07-loops/01-factorial-or-sum/.exercise_test.go
+++ 07-loops/01-factorial-or-sum/.exercise_test.go
@@ -8,9 +8,18 @@ import (
 
 func TestCalcFactorialOrSum(t *testing.T) {
 	{{if eq (index . "func") "factorial"}}
-	assert.Equal(t, calcFactorial({{index . "input"}}), {{index . "output"}})
+	assert.Equal(t, 3628800, calcFactorial(10))
+	assert.Equal(t, 24, calcFactorial(4))
+	assert.Equal(t, 1, calcFactorial(1))
 	{{end}}
 	{{if eq (index . "func") "sum"}}
-	assert.Equal(t, calcSum({{index . "input"}}), {{index . "output"}})
+	assert.Equal(t, 55, calcSum(10))
+	assert.Equal(t, 10, calcSum(4))
+	assert.Equal(t, 1, calcSum(1))
+	{{end}}
+	{{if eq (index . "func") "abs"}}
+	assert.Equal(t, 10, calcSum(10))
+	assert.Equal(t, 4, calcSum(-4))
+	assert.Equal(t, 1, calcSum(-1))
 	{{end}}
 }

4b566d0f6d944216a9b9443aa210e03e655e9314
Gabor Retvari <retvari@tmit.bme.hu>
2023-08-21 19:51:30 +0200

    Fix parseduration

--- 02-types/04-printf/.exercise_test.go
+++ 02-types/04-printf/.exercise_test.go
@@ -1,7 +1,6 @@
 package printer
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
--- 04-type-system/01-construct-duration/.exercise_test.go
+++ 04-type-system/01-construct-duration/.exercise_test.go
@@ -2,30 +2,12 @@ package constructduration
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func setConverter(s string) time.Duration {
-	switch s {
-	case "hours":
-		return time.Hour
-	case "minutes":
-		return time.Minute
-	case "seconds":
-		return time.Second
-	case "milliseconds":
-		return time.Millisecond
-	case "microsecond":
-		return time.Microsecond
-	default:
-		return 0 // Will alert for checks in input
- 	}
-}
-
 func TestConstructDuration(t *testing.T) {
-	arg1Conv := setConverter ("{{index . "type1"}}")
-	arg2Conv := setConverter ("{{index . "type2"}}")
-	assert.Equal(t, time.Duration({{index . "arg1"}}*arg1Conv+{{index . "arg2"}}*arg2Conv), constructDuration({{index . "arg1"}}, {{index . "arg2"}}))
+	assert.Equal(t, `{{index . "sol1"}}`, constructDuration(1, 2).String())
+	assert.Equal(t, `{{index . "sol2"}}`, constructDuration(4, 7).String())
+	assert.Equal(t, `{{index . "sol3"}}`, constructDuration(12, 23).String())
 }

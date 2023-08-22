905789a74d606d6536e92f6fe1fea90f6d42f3de
Tamas Levai <levait@tmit.bme.hu>
2023-08-22 11:34:08 +0200

    Fix 07-loops/01-factorial-sum-abs

--- 07-loops/01-factorial-sum-abs/.exercise_test.go
+++ 07-loops/01-factorial-sum-abs/.exercise_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCalcFactorialOrSum(t *testing.T) {
+func TestCalcFactorialOrSumOrAbs(t *testing.T) {
 	{{if eq (index . "func") "factorial"}}
 	assert.Equal(t, 3628800, calcFactorial(10))
 	assert.Equal(t, 24, calcFactorial(4))
@@ -18,8 +18,8 @@ func TestCalcFactorialOrSum(t *testing.T) {
 	assert.Equal(t, 1, calcSum(1))
 	{{end}}
 	{{if eq (index . "func") "abs"}}
-	assert.Equal(t, 10, calcSum(10))
-	assert.Equal(t, 4, calcSum(-4))
-	assert.Equal(t, 1, calcSum(-1))
+	assert.Equal(t, 10, calcAbs(10))
+	assert.Equal(t, 4, calcAbs(-4))
+	assert.Equal(t, 1, calcAbs(-1))
 	{{end}}
 }

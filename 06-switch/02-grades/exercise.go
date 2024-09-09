0662b35ae3b3e75a215a8e4e68c0047bcc95a3a3
Tamás Lévai <levaitamas@users.noreply.github.com>
2024-09-09 12:14:11 +0200

    fix(06-switch/02-grades): Fix function name

--- 06-switch/02-grades/exercise.go
+++ 06-switch/02-grades/exercise.go
@@ -3,7 +3,7 @@ package grades
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// gradeExercise returns the grade of a given task percentage
-func gradeExercise(perfent float32) int {
+// gradeExam returns the grade of an exam with the given percentage
+func gradeExam(perfent float32) int {
 	// INSERT YOUR CODE HERE
 }

202c8da20a91e992bc784f5a741b2d3f8c65d592
benjoe1126 <bence.abonyi16@gmail.com>
2025-01-09 17:33:24 +0100

    Created generics homework
    Created two exercises for generics homework
    
    mostly done with 02-functional
    
    deleted templated readme and removed package from exercise_test.go
    
    fixed comma issue, 01-sorting is completed
    
    started generics homework

---
+++ 24-generics/01-sorting/exercise.go
@@ -0,0 +1,7 @@
+package sorting
+
+// DO NOT REMOVE THIS COMMENT
+//go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+// INSERT YOUR CODE HERE
+func sortSlice(input []interface{}) []interface{} {}

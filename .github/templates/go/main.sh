aecab2b07111eb542c59c9795cca09ea205cdda0
benjoe <bence.abonyi16@gmail.com>
2025-05-24 21:55:15 +0200

    updated go action script to not have code duplication and removed apply_and_wait

--- .github/templates/go/main.sh
+++ .github/templates/go/main.sh
@@ -1,25 +1,17 @@
 cd "${PROJECT_PATH}"/99-labs/code || exit 1
 if [ "${TESTS}" = "all" ]; then
-  items=("helloworld" "splitdim", "kvstore")
-  for item in "${items[@]}"; do
-    pushd . > /dev/null
-    cd "${item}" && apply_and_wait && go mod download &&  go test ./... --tags=kubernetes --count 1
-    ret=$?
-    if [[ $ret -ne 0 ]]; then
-      exit $ret
-    fi
-    popd > /dev/null
-  done
-else
-  for TEST in ${TESTS}; do
-    pushd . > /dev/null
-    cd ${TEST} || exit 1
-    go mod download
-    go test ./... --tags=kubernetes --count 1
-    ret=$?
-    if [[ $ret -ne 0 ]]; then
-      exit $ret
-    fi
-    popd > /dev/null
-  done
+  TESTS=("helloworld" "splitdim", "kvstore")
 fi
+
+for TEST in ${TESTS}; do
+  pushd . > /dev/null
+  cd ${TEST} || exit 1
+  go mod download
+  go test ./... --tags=kubernetes --count 1
+  ret=$?
+  if [[ $ret -ne 0 ]]; then
+    exit $ret
+  fi
+  popd > /dev/null
+done
+

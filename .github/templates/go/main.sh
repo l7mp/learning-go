6347feda30c30ce642b8fc40710680b4205a2301
benjoe <bence.abonyi16@gmail.com>
2025-05-24 22:10:10 +0200

    fixed go action main.sh to actually iterate through all tests

--- .github/templates/go/main.sh
+++ .github/templates/go/main.sh
@@ -1,9 +1,9 @@
 cd "${PROJECT_PATH}"/99-labs/code || exit 1
 if [ "${TESTS}" = "all" ]; then
-  TESTS=("helloworld" "splitdim", "kvstore")
+  TESTS=("helloworld" "splitdim" "kvstore")
 fi
 
-for TEST in ${TESTS}; do
+for TEST in ${TESTS[@]}; do
   pushd . > /dev/null
   cd ${TEST} || exit 1
   go mod download
